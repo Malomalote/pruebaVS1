6e1187696b64e08a5e306052e9a53a82eeb63cef
antonio <amigarci@hotmail.com>
2017-05-19 00:26:06 +0200

    sdffsf

--- pruebaCoreNET1/Startup.cs
+++ pruebaCoreNET1/Startup.cs
@@ -30,7 +30,7 @@ namespace pruebaCoreNET1
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Hello Worldbe eg!");
             });
         }
     }
